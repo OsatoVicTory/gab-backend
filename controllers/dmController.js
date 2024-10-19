c6496c1a0d8e3cd075d814db108aea3272fd5c41
Tory <76063134+OsatoVicTory@users.noreply.github.com>
2024-10-19 07:41:04 +0100

    Update dmController.js

--- controllers/dmController.js
+++ controllers/dmController.js
@@ -28,7 +28,7 @@ exports.scrappedData = catchAsync(async (req, res) => {
     // Open a new page
     const page = await browser.newPage();
 
-    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36");
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Firefox/91.0");
     await page.setExtraHTTPHeaders({
         'accept-language': 'en-US,en;q=0.9',
         'referer': 'https://www.recaptcha.net/',
